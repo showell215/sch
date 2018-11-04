884effd8f28cebe53590f0f640f31f3983ac2b48
Steven Howell <showell@us.ibm.com>
2018-11-04 09:03:05 -0500

    cleanup

--- deployPrep.sh
+++ deployPrep.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-
 . build.sh
 cp static.json dist
-# cd dist
 sed -i 's/dist/#dist/' \.gitignore
-# git add  dist/
-# git commit -m deployment
 cd dist
-# # git subtree push -f --prefix dist heroku master
-# git push heroku `git subtree split --prefix dist master`:master --force
-# sed -i '' 's/#dist/dist/' \.gitignore
