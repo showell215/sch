29043a735217f5a1dfb2d3a538ff47562f79a131
Steven Howell <showell@us.ibm.com>
2018-11-04 08:00:06 -0500

    text color

--- deployPrep.sh
+++ deployPrep.sh
@@ -2,10 +2,11 @@
 
 . build.sh
 cp static.json dist
+# cd dist
+sed -i 's/dist/#dist/' \.gitignore
+# git add  dist/
+# git commit -m deployment
 cd dist
-sed -i '' 's/dist/#dist/' \.gitignore
-git add  dist/
-git commit -m deployment
 # # git subtree push -f --prefix dist heroku master
 # git push heroku `git subtree split --prefix dist master`:master --force
 # sed -i '' 's/#dist/dist/' \.gitignore
